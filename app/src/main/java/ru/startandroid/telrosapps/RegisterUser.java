9667c708b8a6b43db23c2c6e843cd77bcde7213f
dmitry <zhdanovheavy@yandex.ru>
2022-10-22 22:24:14 +0300

    Initial commit

---
+++ app/src/main/java/ru/startandroid/telrosapps/RegisterUser.java
@@ -0,0 +1,20 @@
+package ru.startandroid.telrosapps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterUser extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_user);
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+}
